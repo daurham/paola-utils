4ee4ffc27d3eb3edba4ac993c001cf3f672b446e
Dan Rouse <daniel.rouse@galvanize.com>
2021-07-09 13:56:13 -0400

    google sheets: add metadata accessors

--- googleSheets/index.js
+++ googleSheets/index.js
@@ -24,8 +24,100 @@ async function getRows(worksheet) {
   }, {}));
 }
 
+async function getSheetMetadata(doc, metadataKey) {
+  const res = await doc.axios.post('/developerMetadata:search', {
+    dataFilters: [{
+      developerMetadataLookup: {
+        metadataKey,
+        metadataLocation: {
+          spreadsheet: true,
+        },
+      },
+    }],
+  });
+  if (res.status !== 200 || !res.data || !res.data.matchedDeveloperMetadata) return null;
+  return res.data.matchedDeveloperMetadata[0].developerMetadata.metadataValue;
+}
+
+function createSheetMetadata(doc, metadataKey, metadataValue) {
+  return doc.axios.post(':batchUpdate', {
+    requests: [{
+      createDeveloperMetadata: {
+        developerMetadata: {
+          metadataKey,
+          metadataValue,
+          location: {
+            spreadsheet: true,
+          },
+          visibility: 1,
+        },
+      },
+    }],
+    includeSpreadsheetInResponse: false,
+  });
+}
+
+function updateSheetMetadata(doc, metadataKey, metadataValue) {
+  return doc.axios.post(':batchUpdate', {
+    requests: [{
+      updateDeveloperMetadata: {
+        dataFilters: [{
+          developerMetadataLookup: {
+            metadataKey,
+            metadataLocation: {
+              spreadsheet: true,
+            },
+          },
+        }],
+        developerMetadata: {
+          metadataKey,
+          metadataValue,
+          location: {
+            spreadsheet: true,
+          },
+          visibility: 1,
+        },
+        fields: '*',
+      },
+    }],
+    includeSpreadsheetInResponse: false,
+  });
+}
+
+async function upsertSheetMetadata(doc, metadataKey, metadataValue) {
+  const existingMetadata = await getSheetMetadata(doc, metadataKey);
+  if (!existingMetadata) {
+    return createSheetMetadata(doc, metadataKey, metadataValue);
+  }
+  return updateSheetMetadata(doc, metadataKey, metadataValue);
+}
+
+function deleteSheetMetadata(doc, metadataKey) {
+  return doc.axios.post(':batchUpdate', {
+    requests: [{
+      deleteDeveloperMetadata: {
+        dataFilter: {
+          developerMetadataLookup: {
+            metadataKey,
+            metadataLocation: {
+              spreadsheet: true,
+            },
+          },
+        },
+      },
+    }],
+    includeSpreadsheetInResponse: false,
+  });
+}
+
 module.exports = {
   loadGoogleSpreadsheet,
   replaceWorksheet,
   getRows,
+
+  getSheetMetadata,
+  createSheetMetadata,
+  updateSheetMetadata,
+  upsertSheetMetadata,
+  deleteSheetMetadata,
 };
