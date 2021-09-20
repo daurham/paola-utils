f32c01555d69c1ca724283506e25fcad8dbbe323
Dan Rouse <dan@rouse.house>
2021-09-20 13:27:33 -0400

    update Learn cohort ID for 2111

--- constants.js
+++ constants.js
@@ -20,7 +20,7 @@ module.exports = {
   },
 
   /* Learn */
-  LEARN_COHORT_ID: '2965',
+  LEARN_COHORT_ID: '2975',
   LEARN_API_COHORTS: 'https://learn-2.galvanize.com/api/v1/cohorts/',
 
   /* GitHub */
