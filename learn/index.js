7526c86eea7c27f8318694ef8f5eb94da2009565
Dan Rouse <dan@rouse.house>
2022-07-12 11:35:25 -0500

    learn: fix add cohort return value

--- learn/index.js
+++ learn/index.js
@@ -115,7 +115,7 @@ exports.createNewCohort = async (options) => {
     );
     const json = await response.json();
     if (json.error || json.message) throw new Error(json.error || json.message);
-    return response.uid;
+    return json.uid;
   } catch (error) {
     return error.message;
   }
